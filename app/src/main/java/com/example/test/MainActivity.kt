f15905feee66a95d9024100d97736052af81345a
ihaneul <789_skymert@naver.com>
2021-03-05 02:13:46 +0900

    main

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //jane sky check
+        //jane sky check main
     }
 }
